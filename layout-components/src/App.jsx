091227f324ea9fc4b1ec3c2bf2b81e9a64329f2f
Simone Lopes <sisilopes20@gmail.com>
2025-08-05 21:42:06 +0000

    chore: add split screen pattern example

---
+++ layout-components/src/App.jsx
@@ -0,0 +1,22 @@
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = () => {
+  return <h1 style={{ backgroundColor: "green" }}>Left!</h1>;
+};
+
+const RightHandComponent = () => {
+  return <h1 style={{ backgroundColor: "red" }}>Right!</h1>;
+};
+
+function App() {
+  return (
+    <>
+      <SplitScreen rightWeight={5} leftWeight={1}>
+        <LeftHandComponent />
+        <RightHandComponent />
+      </SplitScreen>
+    </>
+  );
+}
+
+export default App;
---
+++ layout-components/src/SplitScreen.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Pane = styled.div`
+  flex: ${(props) => props.weight};
+`;
+
+export const SplitScreen = ({ leftWeight = 1, rightWeight = 1, children }) => {
+  const [left, right] = children;
+  return (
+    <Container>
+      <Pane weight={leftWeight}>{left}</Pane>
+      <Pane weight={rightWeight}>{right}</Pane>
+    </Container>
+  );
+};
